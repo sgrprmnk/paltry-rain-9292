82c3a8655e54e49e44c0a32a5e1ab6a6cb78a7a1
Moumita Das <dasmoumita.me@gmail.com>
2022-08-25 21:10:27 +0530

    day-3

---
+++ index.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+
+  autoplay:{
+      delay:3000,
+      disableOnInteraction:false,
+  },
+
+loop: true,
+
+// If we need pagination
+pagination: {
+el: '.swiper-pagination',
+clickable:true,
+},
+
+// Navigation arrows
+navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
+
+
+});
